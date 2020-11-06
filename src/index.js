5f502f8cca95fa87bb51eac5b4f85c074f0cff76
Ioannis Siampalias <ioannis.siampalias@gmail.com>
2020-11-06 12:06:03 +0200

    should be working now!

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ const cardsToNumbers = (hand) => {
   return handNumbersOnly.map(Number);
 }
 //Check if all cards have the same color
-const sameColor = (hand) => {
+const isFlush = (hand) => {
   const firstCardSuit = cardSuitChar(hand[0]);
   return hand.every((card) => cardSuitChar(card) === firstCardSuit);
 };
 
 //Check if we have five cards in sequential order
-const pairCheckFive = (hand) => {
+const isStraight = (hand) => {
   const lowestCard = Math.min(...hand.map((card) => cardStrength(card)));
   return (
     cardsToNumbers(hand).includes(lowestCard) &&
@@ -30,57 +30,41 @@ const pairCheckFive = (hand) => {
   ) 
 };
 //Check for the lowest Ace high hand
-const pairCheckFiveAceLow = (hand) => (
+const pairCheckFiveAce = (hand) => (
     cardsToNumbers(hand).includes(14) &&
     cardsToNumbers(hand).includes(2) &&
     cardsToNumbers(hand).includes(3) &&
     cardsToNumbers(hand).includes(4) &&
     cardsToNumbers(hand).includes(5)
 ) 
-//Check for the highest Ace high hand
-const pairCheckFiveAceHigh = (hand) => (
-  cardsToNumbers(hand).includes(14) &&
-  cardsToNumbers(hand).includes(13) &&
-  cardsToNumbers(hand).includes(12) &&
-  cardsToNumbers(hand).includes(11) &&
-  cardsToNumbers(hand).includes(10)
-) 
 
 //Check if a hand has cards of the same number
 const cardsDuplicates = (hand) => {
   let counts = {};
-  cardsToNumbers(hand).forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
-  return counts;
+  cardsToNumbers(hand).forEach((x) => { counts[x] = (counts[x] || 0)+1; });
+  return Object.values(counts);
 }
 
 //Check if we have a Royal Flush
-const isRoyalFlush = (hand) => sameColor(hand) && pairCheckFiveAceHigh(hand);
+const isRoyalFlush = (hand) => isFlush(hand) && isStraight(hand) && highCard(hand) === 14;
 
 //Check if we have a straight flush
-const isStraightFlush = (hand) => sameColor(hand) && (pairCheckFive(hand) || pairCheckFiveAceLow(hand));
+const isStraightFlush = (hand) => isFlush(hand) && (isStraight(hand) || pairCheckFiveAce(hand));
 
 //Check if we have four of a kind
-const isFourOfAKind = (hand) => Object.values(cardsDuplicates(hand)).includes(4)
+const isFourOfAKind = (hand) => cardsDuplicates(hand).includes(4)
 
 //Check if we have a full house
-const isFullHouse = (hand) => Object.values(cardsDuplicates(hand)).includes(3) && Object.values(cardsDuplicates(hand)).includes(2)
-
-//Check if we have a Flush
-const isFlush = (hand) => sameColor(hand) && (!pairCheckFive(hand) && !pairCheckFiveAceLow(hand) && !pairCheckFiveAceHigh(hand))
-// TODO: is Flush actually means that cards are the same color
-
-//Check if we have a straight
-const isStraight = (hand) => !sameColor(hand) && (pairCheckFive(hand) || pairCheckFiveAceLow(hand) || pairCheckFiveAceHigh(hand));
-// TODO: Do we actually care that it's not flush now that we fixed the bug?
+const isFullHouse = (hand) => cardsDuplicates(hand).includes(3) && cardsDuplicates(hand).includes(2)
 
 //Check if we have three of a kind
-const isThreeOfAKind = (hand) => Object.values(cardsDuplicates(hand)).includes(3) && Object.values(cardsDuplicates(hand)).includes(1)
+const isThreeOfAKind = (hand) => cardsDuplicates(hand).includes(3) 
 
 //Check if we have two pairs
-const isTwoPair = (hand) =>  Object.values(cardsDuplicates(hand)).indexOf(2) === 2
+const isTwoPair = (hand) =>  cardsDuplicates(hand)[2] === 2
 
 //Check if we have a pair
-const isPair = (hand) => Object.values(cardsDuplicates(hand)).indexOf(2) === 1
+const isPair = (hand) => cardsDuplicates(hand).includes(2)
 
 //Check for the highest card
 const highCard = (hand) =>
